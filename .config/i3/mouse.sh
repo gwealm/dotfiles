bf36ab0b1b01ea09b2fe0e99f900ecab1214f16b
Guilherme Almeida <me@gui1612.com>
2022-05-08 23:58:06 +0100

    deletes previous dotfiles

--- .config/i3/mouse.sh
+++
@@ -1,4 +0,0 @@
-idd=$(xinput --list | grep 'SynPS/2 Synaptics TouchPad' | awk '{print $6}'| cut -d'=' -f2)
-
-xinput --set-prop $idd 306 1
-xinput --set-prop $idd 314 1
--- .config/polybar/scripts/display_dunst_mute.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-state=$(dunstctl is-paused)
-
-if $state; then
-    echo ""
-else
-    echo ""
-fi
